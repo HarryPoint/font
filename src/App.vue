241155295b23b2d053f41d4b8e58a78bcb0f874f
HarryPoint <1751826104@qq.com>
2019-12-09 17:50:41 +0800

    测试修改

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Hom2e</router-link> |
+      <router-link to="/">Homexxxx</router-link> |
       <router-link to="/about">About</router-link>
     </div>
     <router-view/>
